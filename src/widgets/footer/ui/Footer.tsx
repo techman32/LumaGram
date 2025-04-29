989be82a7167481cac1fbd98689bfc1c96d44fa8
alexander.klochko <klochko977@gmail.com>
2025-04-29 22:48:14 +0300

    Поправил flex контейнер футера при адаптивности

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -7,7 +7,7 @@ export default async function Footer() {
 
   return (
     <footer className="border-t border-gray-200 dark:border-white/20">
-      <div className="container px-4 mx-auto py-1 flex items-center justify-between">
+      <div className="container px-4 mx-auto py-1 gap-x-2 flex flex-wrap items-center justify-between">
         <p className="italic text-sm">{t('copyright', { date: new Date(Date.now()).getFullYear() })}</p>
         <div className="flex items-center gap-4">
           <ThemeToggle />
