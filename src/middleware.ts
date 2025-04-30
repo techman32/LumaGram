24911d99c2bb9021866980b9853ac8364fcd2e68
alexander.klochko <klochko977@gmail.com>
2025-04-30 16:44:03 +0300

    Middleware с заделом на будущее

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
+import { NextRequest } from 'next/server'
 
-export default createMiddleware(routing)
+export default async function middleware(request: NextRequest) {
+  const intlMiddleware = createMiddleware(routing)
+  return intlMiddleware(request)
+}
 
 export const config = {
   matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
