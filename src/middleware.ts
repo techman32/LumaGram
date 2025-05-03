d7f718fbdd1212b0b4a791e5d064167d9d207273
alexander.klochko <klochko977@gmail.com>
2025-05-03 20:04:18 +0300

    Актуализация username в middleware для избежания подбмены cookie вручную

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
 import { NextRequest, NextResponse } from 'next/server'
+import { sendRequest } from '@/shared/api/api'
 
 const intlMiddleware = createMiddleware(routing)
 
@@ -10,9 +11,41 @@ export default async function middleware(request: NextRequest) {
 
   const pathname = request.nextUrl.pathname
   const refreshToken = request.cookies.get('refreshToken')?.value
+  const accessToken = request.cookies.get('accessToken')?.value
+  const cookieUsername = request.cookies.get('username')?.value
 
   const isAuthPage = pathname === `/${locale}/auth`
   const isProtectedPage = pathname.startsWith(`/${locale}/`) && !isAuthPage
+
+  if (accessToken) {
+    try {
+      const res = await sendRequest<{ username: string }>('users/check', {
+        method: 'GET',
+        token: accessToken,
+      })
+
+      if (res.username) {
+        const { username: realUsername } = res
+
+        if (!cookieUsername || cookieUsername !== realUsername) {
+          const next = NextResponse.next()
+          next.cookies.set('username', realUsername, {
+            path: '/',
+            sameSite: 'strict',
+          })
+          return next
+        }
+      } else {
+        const next = NextResponse.next()
+        next.cookies.delete('username')
+        return next
+      }
+    } catch {
+      const next = NextResponse.next()
+      next.cookies.delete('username')
+      return next
+    }
+  }
 
   if (refreshToken && isAuthPage) {
     const username = request.cookies.get('username')?.value
