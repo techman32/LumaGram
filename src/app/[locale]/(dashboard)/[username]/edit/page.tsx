0d0f46ba3975fb16b104ba5abfcac59bfbd35f3b
alexander.klochko <klochko977@gmail.com>
2025-05-04 13:11:45 +0300

    Поправил реализацию кнопки "Назад"

--- src/app/[locale]/(dashboard)/[username]/edit/page.tsx
+++ src/app/[locale]/(dashboard)/[username]/edit/page.tsx
@@ -1,11 +1,20 @@
 import EditProfile from '@/widgets/editProfile/ui/EditProfile'
 import ReturnButton from '@/features/returnButton/ui/ReturnButton'
+import { cookies } from 'next/headers'
+import { notFound } from 'next/navigation'
 
 export default async function EditProfilePage() {
+  const cookieStore = await cookies()
+  const cookieUsername = cookieStore.get('username')?.value
+
+  if (!cookieUsername) {
+    notFound()
+  }
+
   return (
     <div className="relative h-full overflow-scroll container mx-auto px-4">
       <div className="fixed top-16">
-        <ReturnButton />
+        <ReturnButton backUrl={`/${cookieUsername}`} />
       </div>
       <EditProfile />
     </div>
--- src/features/returnButton/ui/ReturnButton.tsx
+++ src/features/returnButton/ui/ReturnButton.tsx
@@ -1,15 +1,15 @@
 'use client'
-import { useRouter } from 'next/navigation'
 import Button from '@/shared/ui/Button'
 import { ArrowLeft } from 'lucide-react'
 import { useTranslations } from 'next-intl'
+import { useRouter } from '@/i18n/navigation'
 
-export default function ReturnButton() {
+export default function ReturnButton({ backUrl }: { backUrl: string }) {
   const t = useTranslations('Other')
   const router = useRouter()
 
   return (
-    <Button onClick={() => router.back()}>
+    <Button onClick={() => router.replace(backUrl)}>
       <span className="flex gap-2 items-center">
         <span>
           <ArrowLeft size={16} />
